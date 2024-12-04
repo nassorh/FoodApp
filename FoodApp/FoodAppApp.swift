d5260852388d625f71495f3ec62482826756d194
nassorh <nassorh.dev@gmail.com>
2024-12-04 16:17:40 +0000

    Initial Commit

---
+++ FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by Nassor, Hamad on 04/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
